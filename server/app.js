da43e810d58da094c7b0bfffe9ab44ccaf9a6a4f
hieuhoang0413 <nguyenhieu2072004@gmail.com>
2026-05-27 11:53:39 +0700

    fix cors issue

--- server/app.js
+++ server/app.js
@@ -23,17 +23,7 @@ connectDB();
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-  origin: function (origin, callback) {
-    const allowedOrigins = [
-      "http://localhost:5173", 
-      process.env.CLIENT_URL
-    ];
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
+  origin: true,
   credentials: true
 }));
 
